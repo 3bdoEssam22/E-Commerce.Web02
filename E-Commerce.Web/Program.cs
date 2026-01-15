fdb7a6f7be179e1d2e4d47c501d4b2605b784583
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2026-01-16 00:42:50 +0200

    Add project files.

---
+++ E-Commerce.Web/Program.cs
@@ -0,0 +1,53 @@
+
+using DomainLayer.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Data;
+
+namespace E_Commerce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(Options =>
+            {
+                Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddScoped<IDataSeed, DataSeed>();
+            #endregion
+
+            var app = builder.Build();
+
+            using var scoope = app.Services.CreateScope();
+            var objectOfDataSeed = scoope.ServiceProvider.GetRequiredService<IDataSeed>();
+            objectOfDataSeed.DataSeed();
+
+            #region Configure the HTTP request pipeline.
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            #endregion            
+
+            app.Run();
+        }
+    }
+}
---
+++ Infrastructure/Persistence/DataSeed.cs
@@ -0,0 +1,57 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System.Text.Json;
+
+namespace Persistence
+{
+    public class DataSeed(StoreDbContext _dbContext) : IDataSeed
+    {
+        void IDataSeed.DataSeed()
+        {
+            try
+            {
+                if (!_dbContext.Database.GetPendingMigrations().Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
+                if (!_dbContext.ProductBrands.Any())
+                {
+                    var productBrandData = File.ReadAllText(@"..\Infrastructure\Persistence\Data\DataSeed\brands.json");
+                    var productBrands = JsonSerializer.Deserialize<List<ProductBrand>>(productBrandData);
+                    if (productBrands is not null && productBrands.Any())
+                    {
+                        _dbContext.AddRange(productBrands);
+                    }
+                }
+                if (!_dbContext.ProductTypes.Any())
+                {
+                    var productTypesData = File.ReadAllText(@"..\Infrastructure\Persistence\Data\DataSeed\Types.json");
+                    var productTypes = JsonSerializer.Deserialize<List<ProductType>>(productTypesData);
+                    if (productTypes is not null && productTypes.Any())
+                    {
+                        _dbContext.AddRange(productTypes);
+                    }
+                }
+                if (!_dbContext.Products.Any())
+                {
+                    var productData = File.ReadAllText(@"..\Infrastructure\Persistence\Data\DataSeed\Products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                    if (products is not null && products.Any())
+                    {
+                        _dbContext.AddRange(products);
+                    }
+                }
+                _dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                //TODO
+            }
+        }
+    }
+}
